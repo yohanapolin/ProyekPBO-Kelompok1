6f63c89f11e1ce4fee029bc3d78a248a58bea8fb
letaresaragih <60679635+letaresaragih@users.noreply.github.com>
2020-05-23 22:15:23 +0700

    Create RakBuah.java

---
+++ RakBuah.java
@@ -0,0 +1,13 @@
+public class RakBuah {
+    static private String rak_id;
+    static private int kapasitas;
+            
+    public RakBuah (String _id, int _Kapasitas){
+        rak_id = _id;
+        kapasitas = _Kapasitas;
+    }
+   
+    public String getRak_id(){
+        return rak_id;
+    }
+}
