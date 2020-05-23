f270c07199fa04b4d2e87908134d137fcbe1782f
letaresaragih <60679635+letaresaragih@users.noreply.github.com>
2020-05-23 21:42:54 +0700

    Create RakBuah.java

---
+++ RakBuah.java
@@ -0,0 +1,14 @@
+public class RakBuah {
+    static private String ID;
+    static private int Kapasitas;
+            
+    public RakBuah (String _ID, int _Kapasitas){
+        ID = _ID;
+        Kapasitas = _Kapasitas;
+    }
+    
+    public void disusun (){
+        
+    }
+   
+}
