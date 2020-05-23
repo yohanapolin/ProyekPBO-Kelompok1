d26dae236f16d08bafbeeeb6601a759349b2cf65
yohanapolin <60679725+yohanapolin@users.noreply.github.com>
2020-05-23 20:50:31 +0700

    Create pembeli.java

---
+++ pembeli.java
@@ -0,0 +1,13 @@
+public class Pembeli  {
+    
+    public void MembeliBuah (//akan mengambil class transaksi){   
+    
+    }
+       
+    public void InfoBuah( Buah A){
+        
+        A.TampilkanDataBuah();
+    }   
+        
+    }
+    
