b4ec223bc348a46716a53ea7d1f100e1dc4a1701
dennit0gg <60678648+dennit0gg@users.noreply.github.com>
2020-05-25 13:18:23 +0700

    Create Transaksi.java
    This is the implementation code of Class 'Transaksi'

---
+++ Transaksi.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+/**
+ *
+ * @author dennitogg
+ */
+public class Transaksi {
+    
+    private int nomorTransaksi;
+    private Date tanggalTransaksi;
+    
+    public String simpanTransaksi(){
+        return null;       
+    }
+     
+    public String printTransaksi(){
+        return null;
+    }
+    
+    public void hapusTransaksi(){
+         
+    }
+    
+    public int getNomorTransaksi() {
+        return nomorTransaksi;
+    }
+
+    public Date getTanggalTransaksi() {
+        return tanggalTransaksi;
+    }
+
+    
+}    
