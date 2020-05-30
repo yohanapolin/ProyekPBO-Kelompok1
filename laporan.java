beddf881322b6b2709939774196df4239e15b4de
Naomi Anastasya <60679693+naomicho@users.noreply.github.com>
2020-05-30 12:56:56 +0700

    Update laporan.java

--- laporan.java
+++ laporan.java
@@ -4,38 +4,9 @@ public class Laporan  {
     public String tanggal_laporan;
     public String isi;
     
-
-   public void set_nomor (Integer nomor) {
-       nomor_laporan = nomor;
-   }
-   
-   public void set_jenis (String jenis) {
-        jenis_laporan = jenis;
-   }
-   
-   public void set_tgl (String tanggal) {
-        tanggal_laporan = tanggal;
-   }
-   
-   public void set_isi (String isi_) {
-       isi = isi_;
-   }
-   
-   public Integer get_nomor (Integer nomor) {
-        return nomor;
-   }
-   
-   public String get_jenis (String jenis) {
-       return jenis;
-   }
-   
-   public String get_tanggal (String tanggal) {
-       return tanggal;
-   }
-   
-   public String get_isi (String isi) {
-       return isi;
-   }
-       
-       
-   }
+    Public tulis_laporan (Integer no, String jenis, String tanggal, String isi) {
+    }
+    public String lihat_laporan () {
+        return 1;
+    }
+}
