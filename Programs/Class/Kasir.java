3d3b268d58b9b1be7fc8003bd292aa3a17f73db5
dennit0gg <60678648+dennit0gg@users.noreply.github.com>
2020-06-09 11:47:51 +0700

    Create Kasir.java

---
+++ Programs/Class/Kasir.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+/**
+ *
+ * @author dennitogg
+ */
+public class Kasir {
+    
+    private string id;
+    private string nama;
+    
+    public String mengelolaTransaksiPembayaran(){
+        return null;
+    }
+
+    public String mengelolaLaporan() {
+        return null;
+    }
+    
+}
