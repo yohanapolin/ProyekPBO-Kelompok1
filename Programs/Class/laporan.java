11466f2c16a5258afde6818f54a91880f81c8015
Naomi Anastasya <60679693+naomicho@users.noreply.github.com>
2020-06-09 12:41:27 +0700

    Create laporan.java

---
+++ Programs/Class/laporan.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+@author naomi
+
+public class laporan {
+  private String noLaporan;
+  private String jenisLaporan;
+  private Date tanggalLaporan;
+  
+  public void tulisLaporan (String noLaporan, String jenisLaporan, Date tanggalLaporan) {
+  this.noLaporan = noLaporan;
+  this.jenisLaporan = jenisLaporan;
+  this.tanggalLaporan= tanggalLaporan;
+  }
+  
+  public String lihatLaporan() {
+  return 1;
+  }
+ }
+  
+        
