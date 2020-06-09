dbb0e401696d3b42184d4bd7dc856718925ecb19
Naomi Anastasya <60679693+naomicho@users.noreply.github.com>
2020-06-09 13:30:23 +0700

    Create pemiliktoko.java

---
+++ Programs/Class/pemiliktoko.java
@@ -0,0 +1,20 @@
+@author naomi
+
+public class pemilik_toko {
+  private String nama;
+  private Laporan laporan;
+  private pemesanan_buah pemesananbuah;
+  private Kasir catattransaksi;
+  
+  public String melihatlaporan () {
+    return laporan;
+    }
+  public void memesanbuah (String jenis_buah, int jumlah) {
+  }
+  public String mencatattransaksi () {
+  return catattransaksi;
+  }
+ }
+  
+  
+  
