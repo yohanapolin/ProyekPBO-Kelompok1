a860324b305f5b9eb451c77fae322740699420f8
letaresaragih <60679635+letaresaragih@users.noreply.github.com>
2020-06-09 13:17:24 +0700

    Update laporan.java

--- Programs/Class/laporan.java
+++ Programs/Class/laporan.java
@@ -2,19 +2,19 @@ import java.util.Date;
 
 @author naomi
 
-public class laporan {
+public class Laporan {
   private String noLaporan;
   private String jenisLaporan;
   private Date tanggalLaporan;
   
-  public void tulisLaporan (String noLaporan, String jenisLaporan, Date tanggalLaporan) {
+  public Laporan (String noLaporan, String jenisLaporan, Date tanggalLaporan) {
   this.noLaporan = noLaporan;
   this.jenisLaporan = jenisLaporan;
   this.tanggalLaporan= tanggalLaporan;
   }
   
-  public String lihatLaporan() {
-  return 1;
+  public String getNomor() {
+  return noLaporan;
   }
  }
   
