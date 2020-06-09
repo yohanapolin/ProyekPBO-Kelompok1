bafef00ff5154ec4f5c0493612d05f0f9b139914
yohanapolin <60679725+yohanapolin@users.noreply.github.com>
2020-06-09 12:33:10 +0700

    Create pembeli.java

---
+++ Programs/Class/pembeli.java
@@ -0,0 +1,21 @@
+package ProyekPBO;
+
+public class Pembeli {
+    
+    public void membelibuah(){
+       Transaksi transaksi = new Transaksi();
+       transaksi.getKodePembayaran();
+       
+    }
+    
+   public void checkinfobuah(){
+       Buah buah = new Buah();
+       buah.getID_buah();
+       buah.getName();
+       buah.getkadauarsa();
+       buah.getHarga();
+       
+       
+   }
+    
+}
