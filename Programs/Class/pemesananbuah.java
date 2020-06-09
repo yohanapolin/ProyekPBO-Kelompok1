a290eb268a88fb17a911bc009689cbed1599b6ae
Naomi Anastasya <60679693+naomicho@users.noreply.github.com>
2020-06-09 12:54:53 +0700

    Create pemesananbuah.java

---
+++ Programs/Class/pemesananbuah.java
@@ -0,0 +1,20 @@
+@author naomi
+
+public class pemesanan_buah {
+  private String jenis_buah;
+  private int jumlah;
+  
+  public void memesanbuah (String jenis_buah, int jumlah) {
+  this.jenis_buah = jenis_buah;
+  this.jumlah = jumlah;
+  }
+  
+  public String melihatdatabuah() {
+  return 1;
+  }
+ }
+
+
+
+
+
