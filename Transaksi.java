36f27a17946a10e041b984b9980942f48a5c84c8
dennit0gg <60678648+dennit0gg@users.noreply.github.com>
2020-06-09 11:16:26 +0700

    Update Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -5,8 +5,32 @@ import java.util.Date;
  */
 public class Transaksi {
     
+    private int kodePembayaran;
+    private int totalPembayaran;
+    private String jenisPembayaran;
+    private Date tanggalPembayaran;
     private int nomorTransaksi;
     private Date tanggalTransaksi;
+    
+    public String simpanPembayaran(){
+        return null;
+    }
+
+    public Date getTanggalPembayaran() {
+        return tanggalPembayaran;
+    }
+
+    public int getKodePembayaran() {
+        return kodePembayaran;
+    }
+
+    public void setTotalPembayaran(int totalPembayaran) {
+        this.totalPembayaran = totalPembayaran;
+    }
+
+    public void setJenisPembayaran(String jenisPembayaran) {
+        this.jenisPembayaran = jenisPembayaran;
+    }
     
     public String simpanTransaksi(){
         return null;       
