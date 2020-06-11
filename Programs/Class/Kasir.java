d461abf8351618d72919a8e6f6278be3098958b1
dennit0gg <60678648+dennit0gg@users.noreply.github.com>
2020-06-11 21:22:23 +0700

    Update Kasir.java

--- Programs/Class/Kasir.java
+++ Programs/Class/Kasir.java
@@ -1,21 +1,34 @@
-import java.util.Date;
-
-/**
- *
- * @author dennitogg
- */
 public class Kasir {
     
     private String id;
     private String nama;
-    private Laporan laporan;
-    
-    public String mengelolaTransaksiPembayaran(){
-        return null;
+    private String laporan;
+
+    public void setLaporan(String laporan) {
+        this.laporan = laporan;
     }
 
-    public String mengelolaLaporan() {
+    public String getLaporan() {
         return laporan;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    
     
 }
+
