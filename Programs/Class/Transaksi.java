92b20cd417e0b23135ef41762aa7a983559bf57d
dennit0gg <60678648+dennit0gg@users.noreply.github.com>
2020-06-11 21:21:56 +0700

    Update Transaksi.java

--- Programs/Class/Transaksi.java
+++ Programs/Class/Transaksi.java
@@ -1,42 +1,30 @@
-import java.util.Date;
-/**
- *
- * @author dennitogg
- */
 public class Transaksi {
     
-    private int kodePembayaran;
-    private int totalPembayaran;
-    private int nomorPembayaran;
-    private Date tanggalPembayaran;
+    private int hargaBuah;
+    private int jumlahBuah;
+    private int pembayaran;
+    private int kembalian;
+    private int total;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getKembalian() {
+        return kembalian;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlahBuah = jumlah;
+    }
+
+    public void setHarga(int harga) {
+        this.hargaBuah = harga;
+    }
+
+    public void setBayar(int bayar) {
+        this.pembayaran = bayar;
+    }
     
-    public String simpanPembayaran(){
-        return null;
-    }
-
-    public int getKodePembayaran() {
-        return kodePembayaran;
-    }
-
-    public void setTotalPembayaran(int totalPembayaran) {
-        this.totalPembayaran = totalPembayaran;
-    }
-
-    public void setJenisPembayaran(String jenisPembayaran) {
-        this.jenisPembayaran = jenisPembayaran;
-    }
-     
-    public String printPembayaran(){
-        return null;
-    }
-    
-    public int getNomorPembayaran() {
-        return nomorTransaksi;
-    }
-
-    public Date getTanggalPembayaran() {
-        return tanggalTransaksi;
-    }
-
     
 }    
